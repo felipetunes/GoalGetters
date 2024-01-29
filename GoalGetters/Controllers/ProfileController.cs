19945bca62e555e23569536eacaab31847df43db
Felipe Antunes de Oliveira <felipe.tunes@outlook.com>
2024-01-29 19:50:23 -0300

    country por dropdownlist

--- GoalGetters/Controllers/ProfileController.cs
+++ GoalGetters/Controllers/ProfileController.cs
@@ -7,6 +7,8 @@ using System.Numerics;
 using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 using X.PagedList;
 using X.PagedList.Mvc.Core;
+using System.Globalization;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace GoalGetters.Controllers
 {
@@ -51,17 +53,19 @@ namespace GoalGetters.Controllers
             var team = await _apiServiceTeam.GetById(id);
             var pl = await _apiServiceTeam.GetPlayersByTeamId(id);
 
-            // Ordenar a lista antes de paginar
-            var orderedPlayers = pl.OrderBy(p => p.Position);
-
-            team.PlayersCount = pl.Count();
-            team.AverageWage = pl.Average(p => p.Age).ToString("0.#");
-
-            team.Players = orderedPlayers.ToPagedList(page,11);
-          
-            foreach (var player in team.Players)
+            if (pl != null && pl.Count != 0)
             {
-                player.TeamName = team.Name;
+                // Ordenar a lista antes de paginar
+                var orderedPlayers = pl.OrderBy(p => p.Position);
+
+                team.PlayersCount = pl.Count();
+                team.AverageWage = pl.Average(p => p.Age).ToString("0.#");
+                team.Players = orderedPlayers.ToPagedList(page, 11);
+
+                foreach (var player in team.Players)
+                {
+                    player.TeamName = team.Name;
+                }
             }
             return View(team);
         }
@@ -69,6 +73,9 @@ namespace GoalGetters.Controllers
         // GET: PlayerController/Create
         public ActionResult CreateTeam()
         {
+
+            ViewBag.CountryList = new SelectList(GetCountryNames());
+
             return View();
         }
 
@@ -90,6 +97,8 @@ namespace GoalGetters.Controllers
         // GET: PlayerController/Create
         public ActionResult CreatePlayer()
         {
+            ViewBag.CountryList = new SelectList(GetCountryNames());
+
             return View();
         }
 
@@ -112,16 +121,35 @@ namespace GoalGetters.Controllers
         public async Task<ActionResult> EditPlayer(string entity, int id)
         {
             var player = await _apiServicePlayer.GetById(id);
-            return View(player);
 
+            var selectedCountry = string.IsNullOrEmpty(player.Country) ? "" : player.Country;
+
+            ViewBag.CountryList = new SelectList(GetCountryNames(), selectedCountry);
+
+            return View(player);
         }
 
         // GET: EntityController/Edit/5
         public async Task<ActionResult> EditTeam(string entity, int id)
         {
-                var team = await _apiServiceTeam.GetById(id);
-                return View(team);
+            var team = await _apiServiceTeam.GetById(id);
+
+            var selectedCountry = string.IsNullOrEmpty(team.Country) ? "" : team.Country;
+
+            ViewBag.CountryList = new SelectList(GetCountryNames(), selectedCountry);
+
+            return View(team);
         }
+
+        private IEnumerable<string> GetCountryNames()
+        {
+            var countries = ISO3166.Country.List.Select(c => c.TwoLetterCode);
+            var translatedCountries = countries.Select(country => IsoNames.CountryNames.GetName(new CultureInfo("pt"), country))
+                                               .Where(name => !string.IsNullOrEmpty(name))
+                                               .OrderBy(name => name);
+            return translatedCountries;
+        }
+
 
         // POST: EntityController/Edit/5
         [HttpPost]
